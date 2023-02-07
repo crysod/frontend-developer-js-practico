c1a28f6e364e5e67c8dc31c1ccc373b8bfc9df4b
Cristian F. Ortega Diaz <crysod@gmail.com>
2023-02-06 20:33:30 -0500

    Integración del menu mobile y ajustes de responsividad en los menus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const iconMenuMobile = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+iconMenuMobile.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive')
 }
