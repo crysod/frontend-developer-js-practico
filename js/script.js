870e82fb5f399287503c4e9bb4f7aca5b6afacc0
Cristian F. Ortega Diaz <crysod@gmail.com>
2023-02-06 21:33:21 -0500

    Integración la vista de detalle del carrito y ajustes al abrir un menu no copalse con otro

--- js/script.js
+++ js/script.js
@@ -4,12 +4,31 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenuMobile = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 
+const navbarShoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click',toggleDesktopMenu);
 iconMenuMobile.addEventListener('click',toggleMobileMenu);
+navbarShoppingCart.addEventListener('click',toggleDetailcar);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    if (!productDetail.classList.contains('inactive')) {
+        productDetail.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
-    menuMobile.classList.toggle('inactive')
+    if (!productDetail.classList.contains('inactive')) {
+        productDetail.classList.add('inactive');
+    }
+    menuMobile.classList.toggle('inactive');
+}
+function toggleDetailcar() {
+    if (!menuMobile.classList.contains('inactive')) {
+        menuMobile.classList.add('inactive');
+    }
+    if(!desktopMenu.classList.contains('inactive')){
+        desktopMenu.classList.add('inactive');
+    }
+    productDetail.classList.toggle('inactive');
 }
