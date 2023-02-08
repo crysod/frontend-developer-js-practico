30d55de1476b228dde9eecb94c97933b5fd2c898
Cristian F. Ortega Diaz <crysod@gmail.com>
2023-02-07 19:47:11 -0500

    Integacion del Aside detalle de producto y ajustes de CSS y conflictos entre los menos.

--- js/script.js
+++ js/script.js
@@ -4,14 +4,18 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenuMobile = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+
 const navbarShoppingCart = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetail = document.querySelector('#productDetail');
 
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 iconMenuMobile.addEventListener('click',toggleMobileMenu);
 navbarShoppingCart.addEventListener('click',toggleDetailcar);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
 function toggleDesktopMenu() {
     if (!shoppingCartContainer.classList.contains('inactive')) {
@@ -23,6 +27,7 @@ function toggleMobileMenu() {
     if (!shoppingCartContainer.classList.contains('inactive')) {
         shoppingCartContainer.classList.add('inactive');
     }
+    closeProductDetailAside();
     menuMobile.classList.toggle('inactive');
 }
 function toggleDetailcar() {
@@ -32,7 +37,17 @@ function toggleDetailcar() {
     if(!desktopMenu.classList.contains('inactive')){
         desktopMenu.classList.add('inactive');
     }
+    if(!productDetail.classList.contains('inactive')){
+        productDetail.classList.add('inactive');
+    }
     shoppingCartContainer.classList.toggle('inactive');
+}
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetail.classList.remove('inactive');
+}
+function closeProductDetailAside(){
+    productDetail.classList.add('inactive');
 }
 
 const productList = [];
@@ -43,27 +58,27 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 productList.push({
-    name: 'Bike',
+    name: 'Mouse',
+    price: 200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Compu',
+    price: 2200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'keyboard',
+    price: 500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'touch pad',
     price: 1200,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
 productList.push({
-    name: 'Bike',
-    price: 1200,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
-    name: 'Bike',
-    price: 1200,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
-    name: 'Bike',
-    price: 1200,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
-    name: 'Bike',
+    name: 'table',
     price: 1200,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 })
@@ -76,6 +91,7 @@ function renderProducts(arr){
     
         const productImg=document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click',openProductDetailAside);
     
         const productInfo=document.createElement('div');
         productInfo.classList.add('product-info');
